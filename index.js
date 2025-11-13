86d19c774e9f31e6e6b67921fb76524e06fe4949
MKmuslim-777 <muslimuddinkaisanmk@gmail.com>
2025-11-12 23:50:22 -0800

    added property update api and comments api

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 const app = express();
 
+// middlewares
+app.use(cors());
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USERR}:${process.env.DB_PASSS}@cluster0.tnbzfze.mongodb.net/?appName=Cluster0`;
 // console.log(process.env.DB_USERR, process.env.DB_PASSS);
 
@@ -20,17 +24,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
 
     const homeNestDB = client.db("home-nest");
     const propertiesCollection = homeNestDB.collection("properties");
+    const usersCollection = homeNestDB.collection("user");
 
-    app.get("/properties", async (req, res) => {
-      const result = await propertiesCollection.find().toArray();
-      res.send(result);
-    });
+    // app.get("/properties", async (req, res) => {
+    //   const result = await propertiesCollection.find().toArray();
+    //   res.send(result);
+    // });
 
     app.get("/properties/:id", async (req, res) => {
       const id = req.params.id;
@@ -52,13 +57,64 @@ async function run() {
       res.send(result);
     });
 
-    // app.post("/products", verifyFireBaseToken, async (req, res) => {
-    //   const authorization = req.headers.authorization;
-    //   // console.log("after headers in the post", authorization);
-    //   const newProduct = req.body;
-    //   const result = await productsCollection.insertOne(newProduct);
-    //   res.send(result);
-    // });
+    app.get("/properties", async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { "postedBy.email": email };
+      }
+
+      const cursor = propertiesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/comments", async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { "commentBy.email": email };
+      }
+
+      const cursor = propertiesCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Users apis
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        res.send({
+          message: "user already exits. do not need to insert again",
+        });
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
+
+    // Put
+    app.put("/properties/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const objectId = new ObjectId(id);
+      // console.log(id);
+      // console.log(data);
+
+      const filter = { _id: objectId };
+      const updateData = {
+        $set: data,
+      };
+
+      const result = await propertiesCollection.updateOne(filter, updateData);
+      console.log(result);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -69,10 +125,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-// middlewares
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello Home Nest");
